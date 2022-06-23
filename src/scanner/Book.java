77c2969ac3ec2f61dd41f810cbf61461ed61eda4
Uros Dragojevic <dragojevic.u@gmail.com>
2022-06-23 19:01:38 +0200

    Initial commit.

---
+++ src/scanner/Book.java
@@ -0,0 +1,60 @@
+package scanner;
+
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+    private int pages;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public Book() {
+    }
+
+    public Book(String title, String author, int year, int pages) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.pages = pages;
+    }
+
+    @Override
+    public String toString() {
+        return "Knjiga{" +
+                "naziv='" + title + '\'' +
+                ", autor='" + author + '\'' +
+                ", godina=" + year +
+                ", br. strana=" + pages +
+                '}' + '\n';
+    }
+}
---
+++ src/scanner/Main.java
@@ -0,0 +1,34 @@
+package scanner;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static List<Book> loadBooks(String fileName){
+        ArrayList<Book> books = new ArrayList<>();
+        try {
+            Scanner sc = new Scanner(new File(fileName));
+            while(sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] data = line.split(", ");
+                String title = data[0];
+                String author = data[1];
+                int year = Integer.parseInt(data[2]);
+                int pages = Integer.parseInt(data[3]);
+                Book k = new Book(title, author, year, pages);
+                books.add(k);
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return books;
+    }
+    public static void main(String[] args) {
+        List<Book> books = loadBooks("knjige.txt");
+        System.out.println(books);
+    }
+}
